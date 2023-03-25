48bfa11fad4441b2e44537e4cb7d0907ffb1ce8f
Tana Luciya joji <tana.joji@gmail.com>
2023-03-25 11:19:12 +0100

    ssl

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,37 @@
+import { useState } from 'react'
 import './Login.css'
 
 const Login = () => {
+    const [email, SetEmail] = useState('')
+    const [password, SetPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false);
+
+    const LoginUser = async (e) => {
+        e.preventDefault();
+        
+        const response = await fetch('http://localhost:1337/api/users/auth', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+        console.log(data.token)
+
+		if (data.token) {
+			localStorage.setItem('token', data.token)
+			alert('Login successful')
+			window.location.href = '/dashboard'
+		} else {
+			alert('Please check your username and password')
+		}
+    }
+
     return (
         <>
         <h1 className='header'>ZUTE</h1>
@@ -12,20 +43,24 @@ const Login = () => {
                 <div className='titleContainer'>
                     <p className="title">Login</p>
                 </div>
-                <form >
+                <form onSubmit={LoginUser}>
                     <input 
-                        type="email" id="usrEmail" className="authField" placeholder="Email" 
+                        onChange={(e) => SetEmail(e.target.value)}
+                        type="email" 
+                        className="authField" 
+                        placeholder="Email" 
                         required />
-                    <input type="password"
-                        id="usrPwd" className="authField" placeholder="Password"
+                    <input 
+                        type={showPassword ? "text" : "password"} 
+                        onChange={(e) => SetPassword(e.target.value)}
+                        className="authField" placeholder="Password"
                         required />
                     <div className="showPasswordContainer">
-                        <input type="checkbox" id="showPwd" />
+                        <input type="checkbox" id="showPwd" onChange={(e) => setShowPassword(e.target.checked)}/>
                         <label htmlFor="showPwd">Show password</label>
                     </div>
                     <input type="submit" className="submitButton" value="Continue" />
                 </form>
-                <div id="signInDiv"></div>
             </div>
             <iframe width="auto" height="315" src="https://www.youtube.com/embed/o7Yof1oaASk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
