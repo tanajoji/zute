5dbf230fc3d7ded6ebbe4ff682309183fb4ab58c
Tana Luciya joji <tana.joji@gmail.com>
2023-03-29 09:47:21 +0200

    scroll

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -75,69 +75,71 @@ const Login = () => {
 
     return (
         <>
-        <h1 className='header'>ZUTE</h1>
-        {/* <video autoPlay muted loop id="myVideo">
-            <source src={"https://www.youtube.com/embed/o7Yof1oaASk"} type="video/mp4"/>
-        </video> */}
-        <div className='vfContainer'>
-            <div className="formContainer">
-                <div className='titleContainer'>
-                    <p className="title">{loginOrSignup ? "Login" : "Sign Up"}</p>
-                </div>
-                <form onSubmit={LoginUser}>
-                    {loginOrSignup ?
-                        ""
-                    : 
-                        <>
-                        <input 
-                            onChange={(e) => setFirstName(e.target.value)}
-                            type="text" 
-                            id="firstName" 
-                            className="authField" 
-                            placeholder="Firstname" 
-                            required />
-                        <input 
-                            onChange={(e) => setLastName(e.target.value)}
-                            type="text" 
-                            id="lastName" 
-                            className="authField" 
-                            placeholder="Lastname" 
-                            required />
-                        </> 
-                    }
-                    <input 
-                        onChange={(e) => SetEmail(e.target.value)}
-                        type="email" 
-                        className="authField" 
-                        placeholder="Email" 
-                        required />
-                    <input 
-                        type={showPassword ? "text" : "password"} 
-                        onChange={(e) => SetPassword(e.target.value)}
-                        className="authField" placeholder="Password"
-                        required />
-                    <div className="showPasswordContainer">
-                        <input type="checkbox" id="showPwd" onChange={(e) => setShowPassword(e.target.checked)}/>
-                        <label htmlFor="showPwd">Show password</label>
+        <div className='landingFrame'>
+            <h1 className='header'>ZUTE</h1>
+            {/* <video autoPlay muted loop id="myVideo">
+                <source src={"https://www.youtube.com/embed/o7Yof1oaASk"} type="video/mp4"/>
+            </video> */}
+            <div className='vfContainer'>
+                <div className="formContainer">
+                    <div className='titleContainer'>
+                        <p className="title">{loginOrSignup ? "Login" : "Sign Up"}</p>
                     </div>
-                    <button type="submit" className="submitButton">{
-                        isLoading ?
-                            <img src={Loading} alt="Loading" className="loadingIcon"/>
-                        :
-                            <p>Continue</p>
-                        }                
-                    </button>
-                </form>
+                    <form onSubmit={LoginUser}>
+                        {loginOrSignup ?
+                            ""
+                        : 
+                            <>
+                            <input 
+                                onChange={(e) => setFirstName(e.target.value)}
+                                type="text" 
+                                id="firstName" 
+                                className="authField" 
+                                placeholder="Firstname" 
+                                required />
+                            <input 
+                                onChange={(e) => setLastName(e.target.value)}
+                                type="text" 
+                                id="lastName" 
+                                className="authField" 
+                                placeholder="Lastname" 
+                                required />
+                            </> 
+                        }
+                        <input 
+                            onChange={(e) => SetEmail(e.target.value)}
+                            type="email" 
+                            className="authField" 
+                            placeholder="Email" 
+                            required />
+                        <input 
+                            type={showPassword ? "text" : "password"} 
+                            onChange={(e) => SetPassword(e.target.value)}
+                            className="authField" placeholder="Password"
+                            required />
+                        <div className="showPasswordContainer">
+                            <input type="checkbox" id="showPwd" onChange={(e) => setShowPassword(e.target.checked)}/>
+                            <label htmlFor="showPwd">Show password</label>
+                        </div>
+                        <button type="submit" className="submitButton">{
+                            isLoading ?
+                                <img src={Loading} alt="Loading" className="loadingIcon"/>
+                            :
+                                <p>Continue</p>
+                            }                
+                        </button>
+                    </form>
+                </div>
+                <p>
+                {   loginOrSignup ? 
+                    <>New to ZUTE ? <span className="switchPageLink" onClick={toggleLoginSignup}>Sign up</span></>
+                    :
+                    <>Go to  <span className="switchPageLink" onClick={toggleLoginSignup}>Login</span></>
+                }
+                </p>
+                <img className="cardImage" src={ZuteCard} alt="Card Images"></img>
+                {/* <iframe width="auto" height="315" src="https://www.youtube.com/embed/o7Yof1oaASk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
             </div>
-            <p>
-            {   loginOrSignup ? 
-                <>New to ZUTE ? <span className="switchPageLink" onClick={toggleLoginSignup}>Sign up</span></>
-                :
-                <>Go to  <span className="switchPageLink" onClick={toggleLoginSignup}>Login</span></>
-            }
-            </p>
-            <img className="cardImage" src={ZuteCard} alt="Card Images"></img>
-            {/* <iframe width="auto" height="315" src="https://www.youtube.com/embed/o7Yof1oaASk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
         </div>
         </>
     )
