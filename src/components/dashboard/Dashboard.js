eda9ce4dfe63f74019d8b72ae43064359ce23a38
Tana Luciya joji <tana.joji@gmail.com>
2023-04-30 12:01:59 +0200

    data load from database

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,29 +1,68 @@
 import './Dashboard.css'
 import Dp from "../../assets/steff.jpg"
+import React,{useEffect, useState} from 'react';
+
+
+
 
 
 const Dashboard = () => {
+    const [data,setData] = useState(null)
+
+    const GetData = async () => {
+        const response = await fetch('https://zute.onrender.com/api/users/userlinks', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'jwt': localStorage.getItem('token')
+                },
+            })
+
+        const data = await response.json()
+        console.log(data)
+        console.log(data.firstname)
+        if(data!= null){
+            setData(data)
+        }
+        else{
+            console.log("error")
+        }
+    }
+
+
+    useEffect(()=>{
+        GetData()
+    },[])
+
+
+
     return(
-        <>
-        <div className='dashboardFrame'>
+        ((data !== null) && <>
+        <DashData data={data}/>
+        </>)
+    )
+}
+
+const DashData = (props) => {
+    return(
+        ((props !== null) && <div className='dashboardFrame'>
             <div className='profileSection'>
                 <div>
                     <div className='profilePic' style={{backgroundImage: `url(${Dp})`}}>
                     </div>
                 </div>
                 <div className='profileContents'>
-                    <h1 className='userName'>Steffin Abraham</h1>
+                    <h1 className='userName'>{props.data.firstname} {props.data.lastname}</h1>
                     <div>
-                        <p className='userData'>+33 768 15 4455</p>
-                        <p className='userData'>st3ffin@gmail.com</p>
+                        <p className='userData'>{props.data.phone}</p>
+                        <p className='userData'>{props.data.email}</p>
                     </div>
                 </div>
             </div>
             <div className='linksSection'>
 
             </div>
-        </div>
-        </>
+        </div>)
     )
 }
 
